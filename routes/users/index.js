65d60880a2c12572d38926a0e4e2e3cb432d7b93
Mohtashim Javed <hafizmohtashim3157@gmail.com>
2026-04-02 12:36:09 +0500

    updated

--- routes/users/index.js
+++ routes/users/index.js
@@ -4,12 +4,15 @@ import createUser from "./post.js";
 import loginUser from "./login.js";
 import tokenVerification from "../../config/tokenVerification.js";
 import refresh from "./refresh.js";
+import accessVerification from "./accessVerification.js";
 
 const userRouter = express.Router()
 userRouter.get("/", tokenVerification, getUser);
 userRouter.post("/create", createUser);
 userRouter.post("/login", loginUser);
 userRouter.post("/refresh", refresh);
+userRouter.post("/token-verify", tokenVerification, accessVerification)
+
 
 
 export default userRouter;
